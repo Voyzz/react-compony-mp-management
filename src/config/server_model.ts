752ec784e08f3bbde1e3ae99fdd9c408586d4546
Voyz <18217501371@163.com>
2020-12-30 16:08:31 +0800

    [dev]config_model

--- src/config/server_model.ts
+++
@@ -1,6 +0,0 @@
-const server:{[key:string]:any} = {
-    baseUrl:'',
-    // baseUrl:'http://localhost:3000/',
-}
-
-export default server;
--- src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-import {lazy} from 'react';
-
-const Admin = lazy(() => import('./Admin'));
-
-export {
-    Admin
-}
